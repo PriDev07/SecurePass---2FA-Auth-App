fc3747f2512e427243a122f0acb2e30892b19e08
PriDev07 <lohaniprince72@gmail.com>
2025-06-08 13:33:30 +0530

    Login-screen-added

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:securepass/widgets/custom_button.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Text(
+                  "Log in",
+                  style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                alignment: Alignment.center,
+                child: Image.asset("assets/manwithcomputer.png", scale: 20),
+              ),
+              SizedBox(height: 20),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 8),
+                child: Text(
+                  "Phone number",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              SizedBox(height: 5),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 8),
+                child: TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.call),
+                    hintText: "Enter your phone number",
+                    hintStyle: TextStyle(color: Colors.grey),
+                    contentPadding: EdgeInsets.all(4),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 8),
+                child: Text(
+                  "Phone number",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              SizedBox(height: 5),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 8),
+                child: TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock),
+                    hintStyle: TextStyle(color: Colors.grey),
+                    hintText: "Password",
+                    suffixIcon: Icon(Icons.visibility_off_outlined),
+                    contentPadding: EdgeInsets.all(4),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 8),
+                alignment: Alignment.topRight,
+                child: GestureDetector(
+                  child: Text(
+                    "Forgot password?",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              GestureDetector(child: CustomButton(value: "Log in")),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Don't have an account?",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(width: 10),
+                  GestureDetector(
+                    child: Text(
+                      "Sign up",
+                      style: TextStyle(color: Colors.blueAccent, fontSize: 16),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
