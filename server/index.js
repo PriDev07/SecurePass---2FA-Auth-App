94f31792040000db3c3e45139ce08080ee0819c0
PriDev07 <lohaniprince72@gmail.com>
2025-06-09 21:17:33 +0530

    env-added

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
@@ -5,13 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth:{
-        user: "codesavvy2@gmail.com",
-        pass: "pass"
+        user: process.env.EMAIL,
+        pass: process.env.PASS,
     }
 });
 app.post("/send-otp", async (req,res) =>{
@@ -19,14 +18,14 @@ app.post("/send-otp", async (req,res) =>{
     const mailOptions = {
         from: '"SecurePass" <codesavvy2@gmail.com>"',
         to: email,
-        Subject: "Your otp code",
+        subject: "Your otp code",
         text: `Your OTP code is: ${otp}. It will expire in 5 minutes.`,
 
     };
     try {
         await transporter.sendMail(mailOptions);
         res.status(200).json({success : true});
-    } catch (error) {
+    } catch (err) {
         console.error("Error sending email:", err);
     res.status(500).json({ success: false, error: err.message });
     }
