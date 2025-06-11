5b3ace82717233e197bdbe571738979d464ceb72
PriDev07 <lohaniprince72@gmail.com>
2025-06-11 19:32:29 +0530

    main-updated

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'SecurePass',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
