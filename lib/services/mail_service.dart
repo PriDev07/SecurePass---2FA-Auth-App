4cbc5c56bf223097abaa0adf33452fa580f37531
PriDev07 <lohaniprince72@gmail.com>
2025-06-13 23:48:38 +0530

    some-documentation-added

--- lib/services/mail_service.dart
+++ lib/services/mail_service.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class MailService {
+  //To send mail to the user when they click button
   Future<void> sendOtpEmail(String email, String otp) async {
     final response = await http.post(
       Uri.parse("https://securepass-2fa-auth-app.onrender.com/send-otp"),
